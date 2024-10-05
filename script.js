9a87fbd79a7b730791d9ac93821b794dfbf197ce
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-10-05 11:52:47 +0400

    Add Basic Arithmetic Functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(leftOperand, rightOperand) {
+    return leftOperand + rightOperand;
+}
+
+function subtract(leftOperand, rightOperand) {
+    return leftOperand - rightOperand;
+}
+
+function multiply(leftOperand, rightOperand) {
+    return leftOperand * rightOperand;
+}
+
+function divide(leftOperand, rightOperand) {
+    return leftOperand * rightOperand;
+}
